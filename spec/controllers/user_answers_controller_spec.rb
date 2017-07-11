66b9daf3eb75718767430a9a4eb4804677a4c243
Timothy Robinson <timmyrobby@gmail.com>
2017-07-11 17:44:22 +0100

    Adds user_answers model

---
+++ spec/controllers/user_answers_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UserAnswersController, type: :controller do
+  it "creates a new user answer" do
+    statement = ChatbotStatement.create(message: 'Hello, world!')
+    UserAnswer.create(message: 'Hello, world!', chatbot_statement_id: statement.id)
+    expect(UserAnswer.find_by(message: "Hello, world!")).to be
+  end
+
+  it "does not create a new answer with an invalid chatbot statement id" do
+    UserAnswer.create(message: 'Hello, world!', chatbot_statement_id: 'id')
+    expect(UserAnswer.find_by(message: "Hello, world!")).to be_nil
+  end
+end
