e6705979a4e096b294ea27b6ec204e4d2b819fdd
Timothy Robinson <timmyrobby@gmail.com>
2017-07-12 06:39:32 +0100

    Extracts default starter conversation set up to helper file

--- app/controllers/chatbot_statements_controller.rb
+++ app/controllers/chatbot_statements_controller.rb
@@ -1,4 +1,6 @@
 class ChatbotStatementsController < ApplicationController
+  include ChatbotStatementsHelper
+  
   def new
     @statement = ChatbotStatement.new
     @statement.user_answers.build
@@ -29,49 +31,6 @@ class ChatbotStatementsController < ApplicationController
 
   def statement_params
     params.require(:chatbot_statement).permit(:message, :status, user_answers_attributes: [:message, :response])
-  end
-
-  def load_default_conversation
-    statement = ChatbotStatement.create(message: 'Hey!')
-    UserAnswer.create(
-      message: "How's it going?",
-      response: "Not too bad. You?",
-      chatbot_statement_id: statement.id
-    )
-    UserAnswer.create(
-      message: "Hey! What are you up to?",
-      response: 'Just chilling. You?',
-      chatbot_statement_id: statement.id
-    )
-
-    statement = ChatbotStatement.create(message: 'Not too bad. You?')
-    UserAnswer.create(
-      message: "Yeah I'm good thanks.",
-      response: "Cool.  Well, this is boring... brb",
-      chatbot_statement_id: statement.id
-    )
-    UserAnswer.create(
-      message: "Doing great! Want to browse the web for memes?",
-      response: '...Literally more than anything! :D',
-      chatbot_statement_id: statement.id
-    )
-
-    statement = ChatbotStatement.create(message: 'Just chilling. You?')
-    UserAnswer.create(
-      message: "Yeah me too.",
-      response: "Cool.  Well, this is boring... brb",
-      chatbot_statement_id: statement.id
-    )
-
-    UserAnswer.create(
-      message: "Looking around for the funniest memes",
-      response: 'Haha, ahh I love memes.',
-      chatbot_statement_id: statement.id
-    )
-
-    ChatbotStatement.create(message: '...Literally more than anything! :D', status: 'pass')
-    ChatbotStatement.create(message: 'Haha, ahh I love memes.', status: 'pass')
-    ChatbotStatement.create(message: "Cool.  Well, this is boring... brb", status: 'fail')
   end
 
   def missing_statements?
--- app/helpers/chatbot_statements_helper.rb
+++ app/helpers/chatbot_statements_helper.rb
@@ -1,2 +1,44 @@
 module ChatbotStatementsHelper
+  def load_default_conversation
+    statement = ChatbotStatement.create(message: 'Hey!')
+    UserAnswer.create(
+      message: "How's it going?",
+      response: "Not too bad. You?",
+      chatbot_statement_id: statement.id
+    )
+    UserAnswer.create(
+      message: "Hey! What are you up to?",
+      response: 'Just chilling. You?',
+      chatbot_statement_id: statement.id
+    )
+
+    statement = ChatbotStatement.create(message: 'Not too bad. You?')
+    UserAnswer.create(
+      message: "Yeah I'm good thanks.",
+      response: "Cool.  Well, this is boring... brb",
+      chatbot_statement_id: statement.id
+    )
+    UserAnswer.create(
+      message: "Doing great! Want to browse the web for memes?",
+      response: '...Literally more than anything! :D',
+      chatbot_statement_id: statement.id
+    )
+
+    statement = ChatbotStatement.create(message: 'Just chilling. You?')
+    UserAnswer.create(
+      message: "Yeah me too.",
+      response: "Cool.  Well, this is boring... brb",
+      chatbot_statement_id: statement.id
+    )
+
+    UserAnswer.create(
+      message: "Looking around for the funniest memes",
+      response: 'Haha, ahh I love memes.',
+      chatbot_statement_id: statement.id
+    )
+
+    ChatbotStatement.create(message: '...Literally more than anything! :D', status: 'pass')
+    ChatbotStatement.create(message: 'Haha, ahh I love memes.', status: 'pass')
+    ChatbotStatement.create(message: "Cool.  Well, this is boring... brb", status: 'fail')
+  end
 end
