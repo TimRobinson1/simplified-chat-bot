b663513a92eae8dce317584a379c3b2c613b7839
Timothy Robinson <timmyrobby@gmail.com>
2017-07-12 07:57:30 +0100

    ChatbotStatements now require more than one answer

--- spec/features/page_display_spec.rb
+++ spec/features/page_display_spec.rb
@@ -29,4 +29,10 @@ RSpec.feature "Interface", type: :feature do
     create_statement_and_invalid_answer
     expect(page).to_not have_content 'Test Message'
   end
+
+  it "Does not display a new statement if it has only one given answer" do
+    sign_up
+    create_statement_with_only_one_answer
+    expect(page).to_not have_content 'Test Message'
+  end
 end
--- spec/support/admin_helpers.rb
+++ spec/support/admin_helpers.rb
@@ -1,16 +1,27 @@
 def create_new_statement
   visit '/admin'
   fill_in 'chatbot-statement-input', with: 'Test Message'
-  fill_in 'answer', with: 'My Answer'
-  fill_in 'response', with: 'My Response'
+  fill_in 'answer_one', with: 'My Answer'
+  fill_in 'response_one', with: 'My Response'
+  fill_in 'answer_two', with: 'My Next Answer'
+  fill_in 'response_two', with: 'My Next Response'
+  find('.submit-field input').click
+end
+
+def create_statement_with_only_one_answer
+  visit '/admin'
+  fill_in 'chatbot-statement-input', with: 'Test Message'
+  fill_in 'answer_one', with: 'My Answer'
+  fill_in 'response_one', with: 'My Response'
   find('.submit-field input').click
 end
 
 def create_invalid_statement
-  # No answer present
+  # No answers present
   visit '/admin'
   fill_in 'chatbot-statement-input', with: 'Test Message'
-  fill_in 'response', with: 'My Response'
+  fill_in 'response_one', with: 'My Response'
+  fill_in 'response_two', with: 'My Response'
   find('.submit-field input').click
 end
 
@@ -18,6 +29,7 @@ def create_statement_and_invalid_answer
   # No response present
   visit '/admin'
   fill_in 'chatbot-statement-input', with: 'Test Message'
-  fill_in 'answer', with: 'My Answer'
+  fill_in 'answer_one', with: 'My Answer'
+  fill_in 'answer_two', with: 'My Next Answer'
   find('.submit-field input').click
 end
