929de69a0088ea0ba8d952e33f93cf168ef6514e
Timothy Robinson <timmyrobby@gmail.com>
2017-07-18 17:01:48 +0100

    Removing old/unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'chatbot_statements#chatroom'
 
-  constraints Clearance::Constraints::SignedOut.new do
-    get '/response' => 'user_answers#message'
-    root to: 'chatbot_statements#chatroom'
-  end
+  get '/response' => 'user_answers#message'
 
   constraints Clearance::Constraints::SignedIn.new do
     resources :users, :chatbot_statements, :user_answers
-    get '/admin' => 'clearance/users#dashboard'
+    get '/admin', to: 'clearance/users#dashboard', as: 'admin_route'
     get '/admin/dialogue/new' => 'chatbot_statements#new'
     get '/admin/dialogue/remove' => 'chatbot_statements#remove'
     get '/admin/ending/new' => 'chatbot_statements#ending'
     get '/admin/dialogue/tree' => 'chatbot_statements#view'
-    get '/response' => 'user_answers#message'
-    root to: 'chatbot_statements#chatroom', as: :signed_in_root
   end
 end
